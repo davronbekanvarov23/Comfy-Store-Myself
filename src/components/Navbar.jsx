b4282bf4f4bb396d62ceab7719e79e107c070fa4
davronbekanvarov23 <davronbekanvarov23@gmail.com>
2024-05-27 10:55:57 +0500

    change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,30 +1,20 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import NavLinks from "./NavLinks";
 import Mode from "./Mode";
 import { SlBasket } from "react-icons/sl";
+import { FaBarsStaggered } from "react-icons/fa6";
 function Navbar() {
   return (
     <div className="bg-base-200">
       {" "}
       <div className=" navbar align-content ">
         <div className=" navbar-start">
-          <Link className="btn btn-primary hidden lg:flex text-3xl items-center active">
+          <NavLink className="btn btn-primary hidden lg:flex text-3xl items-center active">
             C
-          </Link>
+          </NavLink>
           <div className="dropdown">
             <label tabIndex="0" className="btn btn-ghost lg:hidden">
-              <svg
-                stroke="currentColor"
-                fill="currentColor"
-                strokeWidth="0"
-                viewBox="0 0 512 512"
-                className="h-6 w-6"
-                height="1em"
-                width="1em"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM64 256c0-17.7 14.3-32 32-32H480c17.7 0 32 14.3 32 32s-14.3 32-32 32H96c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"></path>
-              </svg>
+              <FaBarsStaggered className="h-6 w-6" />
             </label>
             <ul
               tabIndex={0}
@@ -42,7 +32,7 @@ function Navbar() {
 
         <div className=" navbar-end ">
           <Mode />
-          <Link
+          <NavLink
             to="/cart"
             className="indicator btn btn-ghost btn-circle btn-md ml-4"
           >
@@ -52,7 +42,7 @@ function Navbar() {
               </span>
               <SlBasket className="w-7 h-7" />
             </div>
-          </Link>
+          </NavLink>
         </div>
       </div>
     </div>
